9b1eaeaffde3ec4d0e2fe0a9ec4f641809a9175e
Doug Wegscheid <dwegscheid@sbcglobal.net>
2022-11-17 13:52:36 -0500

    First swag putting together ApriltagsFieldLayout.

---
+++ src/main/java/frc/robot/TestAprilTags.java
@@ -0,0 +1,40 @@
+package frc.robot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.apriltag.AprilTag;
+import edu.wpi.first.wpilibj.apriltag.AprilTagFieldLayout;
+
+public class TestAprilTags {
+
+    static private AprilTagFieldLayout aprilTagFieldLayout = null;
+
+    static Rotation3d r3d = new Rotation3d(0, Units.degreesToRadians(-90), 0);
+
+    private static AprilTag makeOne(int id, double y, double z) {
+        y = Units.inchesToMeters(y);
+        z = Units.inchesToMeters(z);
+        Pose3d pose3d = new Pose3d(0.0, y, z, r3d);
+        return new AprilTag (id, pose3d);
+    }
+
+    private static AprilTagFieldLayout make() {
+        List<AprilTag> rv = new ArrayList<>();
+        rv.add (makeOne(1, 9.0, 50.25));
+        rv.add (makeOne(2, 84.0, 51.5));
+        rv.add (makeOne(3, 211.0, 50.5));
+
+        return new AprilTagFieldLayout(rv, 20.0, 10.0);
+    }
+
+    public static AprilTagFieldLayout getAprilTagFieldLayout() {
+        if (aprilTagFieldLayout == null) {
+            aprilTagFieldLayout = make();
+        }
+        return aprilTagFieldLayout;
+    }
+}
---
+++ src/test/java/Test00.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.util.Optional;
+
+import org.junit.Test;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.apriltag.AprilTagFieldLayout;
+import frc.robot.TestAprilTags;
+
+/** Add your docs here. */
+public class Test00 {
+
+    @Test
+    public void t00() throws IOException {
+        AprilTagFieldLayout a = TestAprilTags.getAprilTagFieldLayout();
+        System.out.println(a);
+        a.serialize ("test_apriltags.json");
+
+        System.out.println(a.getTagPose(0).orElse(null));
+        Optional o1 = a.getTagPose(1);
+        System.out.println(o1.get());
+
+    }
+
+    @Test
+    public void t01() {
+        Rotation3d rAboutY = new Rotation3d (0, Units.degreesToRadians(-90), 0);
+        System.out.println (rAboutY);
+        Pose3d p0 = new Pose3d();
+        System.out.println (p0.relativeTo(p0));
+        Pose3d p1 = new Pose3d(0, 0, 0, rAboutY);
+        System.out.println (p1.relativeTo(p0));
+        Pose3d p2 = new Pose3d(1, 1, 1, rAboutY);
+        System.out.println (p2);
+        System.out.println (p2.toPose2d());
+        System.out.println (p2.relativeTo(p0));
+        System.out.println (p2.relativeTo(p1));
+    }
+}
